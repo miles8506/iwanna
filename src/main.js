e6820210d9bb3b860dc1fb13841fddf80598f6a1
miles <a09112332@gmail.com>
2021-11-07 18:26:14 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+// import Vue from 'vue'
+// import App from './App.vue'
+// import router from './router'
+// import store from './store'
 
-//jquery
-import $ from 'jquery'
-Vue.prototype.$ = $
+// //jquery
+// import $ from 'jquery'
+// Vue.prototype.$ = $
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
-// bus
-Vue.prototype.$bus = new Vue();
+// // bus
+// Vue.prototype.$bus = new Vue();
 
-new Vue({
-  router,
-  store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+// new Vue({
+//   router,
+//   store,
+//   render: function (h) { return h(App) }
+// }).$mount('#app')
